85e19014539ece28501ba70da23fcc45f1d86c85
DanAcoVaz <a01736279@tec.mx>
2025-04-12 21:37:00 -0600

    Added comments

--- SuffixArray.py
+++ SuffixArray.py
@@ -1,12 +1,14 @@
 import os
 import re
 
+#Funcion para construir el suffix array
 def build_suffix_array(text):
     suffixes = [(text[i:], i) for i in range(len(text))]
-    suffixes.sort()
+    suffixes.sort() #Ordenacion lexografica
     suffix_array = [s[1] for s in suffixes]
     return suffix_array
 
+#Funcion para construir el LCP Array (Longest Common Prefix) usando Algoritmo de Kasai
 def build_lcp_array(text, suffix_array):
     n = len(text)
     rank = [0] * n
@@ -32,10 +34,11 @@ def build_lcp_array(text, suffix_array):
 
     return lcp
 
+#Funcion para conseguir los substrings comunes entre los dos codigos
 def extract_common_substrings(text, suffix_array, lcp_array, sep_index, min_length = 10):
-    common_substrings = set()
+    common_substrings = set() #Uso de sets en Python, evitando datos duplicados
 
-    for i in range(1, len(text)):
+    for i in range(1, len(text)): #Ignoramos el primer valor del LCP pues sabemos sera 0
         lcp_len = lcp_array[i]
         if lcp_len < min_length:
             continue
@@ -48,11 +51,11 @@ def extract_common_substrings(text, suffix_array, lcp_array, sep_index, min_leng
 
         if in_first != in_second:
             base_start = start1 if in_first else start2
-            for length in range(min_length, lcp_len + 1):
+            for length in range(min_length, lcp_len + 1): #Obtenemos todos los substrings que encontremos
                 substring = text[base_start:base_start + length]
                 common_substrings.add(substring)
         
-    filtered = set()
+    filtered = set() #Filtramos para obtener las cadenas mas largas unicas que se repitan
     common_substrings = sorted(common_substrings, key=lambda s: (-len(s), s))
     for s in common_substrings:
         if not any(s in other and s != other for other in filtered):
@@ -60,10 +63,12 @@ def extract_common_substrings(text, suffix_array, lcp_array, sep_index, min_leng
         
     return sorted(filtered, key=lambda x: (-len(x), x))
 
+#Calculamos porcentaje de similaridad.
 def similarity_score(common_substrings, len_prog1, len_prog2):
     total_common = sum(len(s) for s in common_substrings)
     return (total_common / min(len_prog1, len_prog2)) * 100
 
+#Generamos el reporte en archivo txt
 def generate_report(common_substrings, score, filename):
     with open(filename, "w", encoding="utf-8") as f:
         f.write("=== Subcadenas comunes encontradas ===\n\n")
@@ -71,6 +76,7 @@ def generate_report(common_substrings, score, filename):
             f.write(f"- '{s}' (longitud {len(s)})\n")
         f.write(f"\n=== Porcentaje de similitud: {score:.2f}% ===\n")
 
+#Preprocesamos codigo para compara codigos mas limpios sin tener en cuenta comentarios o formato.
 def preprocesar_codigo(codigo):
     #Comentarios de linea (//, #)
     codigo = re.sub(r'#.*', '', codigo)
@@ -87,7 +93,9 @@ def preprocesar_codigo(codigo):
 
     return codigo
 
+#Pedimos los archivos a comparar, el archivo a escribir el output y la opcion de hacer preprocesamiento o no.
 def comparar_codigos(archivo1, archivo2, output_file, preprocesamiento):
+    #Verificamos que existan los archivos
     if not os.path.exists(archivo1):
         print(f"Error: El archivo '{archivo1}' no existe.")
         return
@@ -95,11 +103,13 @@ def comparar_codigos(archivo1, archivo2, output_file, preprocesamiento):
         print(f"Error: El archivo '{archivo2}' no existe.")
         return
     
+    #Leemos los archivos
     with open(archivo1, "r", encoding="utf-8") as f1:
         codigo1 = f1.read()
     with open(archivo2, "r", encoding="utf-8") as f2:
         codigo2 = f2.read()
     
+    #Si se pidio preprocesamiento, lo hacemos
     if preprocesamiento:
         codigo1 = preprocesar_codigo(codigo1)
         codigo2 = preprocesar_codigo(codigo2)
